50e040e7e556dae5a38626f9a61bbcc94af41d57
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2022-09-08 00:56:11 -0300

    Implementações necessárias para aparição do componente sidebar

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,28 +1,43 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import SidebarHeader from './SidebarHeader';
 import SidebarContent from './SidebarContent';
 import SidebarFooter from './SidebarFooter';
 import SidebarMenuItem from './SidebarMenuItem';
 import {CDBSidebar as Sidebar} from 'cdbreact';
+import manageSessionStorage from '../../helpers/manageSessionStorage';
 import '../../assets/css/Sidebar.css';
+
 
 const SidebarComponent = () => {
 
+  const [sidebarClass, setSidebarClass] = useState('sidebar-wrapper-hidden');
+  
+  useEffect(()=>{
+    if(manageSessionStorage().get('selected')){
+      setSidebarClass('sidebar-wrapper');
+    }
+      
+  },[]);
+  window.addEventListener('continueClicked', () => {
+    setSidebarClass('sidebar-wrapper');
+  });
+
   return (
-    <div id='sidebar-wrapper'>
-      <Sidebar id='sidebar' textColor="#fff" backgroundColor={/*"#2e3035"*/ "#3c3f58" }>
-        <SidebarHeader/>
-        
-        <SidebarContent>
-          <SidebarMenuItem icon="columns" identifier='aboutme'>Sobre mim</SidebarMenuItem>
-          <SidebarMenuItem icon="table" identifier='experience'>Experiências</SidebarMenuItem>
-          <SidebarMenuItem icon="user" identifier='skills'>Skills</SidebarMenuItem>
-          <SidebarMenuItem icon="chart-line" identifier='certifications'>Certificações</SidebarMenuItem>
-        </SidebarContent>
 
-        <SidebarFooter/>
+      <div id={sidebarClass}>
+        <Sidebar id='sidebar' textColor="#fff" backgroundColor={/*"#2e3035"*/ "#2e3035" }>
+          <SidebarHeader/>
+          
+          <SidebarContent>
+            <SidebarMenuItem icon="columns" identifier='aboutme'>Sobre mim</SidebarMenuItem>
+            <SidebarMenuItem icon="table" identifier='experience'>Experiências</SidebarMenuItem>
+            <SidebarMenuItem icon="user" identifier='skills'>Skills</SidebarMenuItem>
+            <SidebarMenuItem icon="chart-line" identifier='certifications'>Certificações</SidebarMenuItem>
+          </SidebarContent>
 
-      </Sidebar>
+          <SidebarFooter/>
+
+        </Sidebar>
     </div>
   );
 };
