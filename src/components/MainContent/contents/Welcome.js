3f3466bff562766a2c80cba43d9a52c3575662ec
gustavo1sad@gmail.com <gustavo1sad@gmail.com>
2022-09-13 02:20:33 -0300

    Separação de texto para boas-vindas em outro componente e implmentado logica para decisão de qual texto será renderizado

---
+++ src/components/MainContent/contents/Welcome.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import Button from 'react-bootstrap/Button';
+import '../../../assets/css/Welcome.css'
+
+const Welcome = (props) => {
+    const [textClass, setTextClass] = useState('slideText-normal');
+
+    function handleClick(){
+        window.dispatchEvent(new Event("continueClicked"));
+        setTextClass('slideText');
+    }
+
+    return (
+        <div className = {textClass}>
+            {props.mode === 'initial' ? 
+                <>
+                    <h4 style={{fontSize: '72px'}}> Olá, tudo bem com você? </h4>
+                    <h3 style={{fontSize: '96px'}}> Me chamo Gustavo </h3>
+                    <h4 style={{fontSize: '72px'}}> Bem-vindo ao meu website :) </h4>
+                    <Button size='lg' onClick={handleClick}>Continuar</Button>
+                </> :
+
+                <>
+                    <h4 style={{fontSize: '48px'}}> Te convido a continuar navegando pelo site</h4>
+                    <h3 style={{fontSize: '52px'}}> para conhecer um pouco mais sobre mim.</h3>
+                    <h4 style={{fontSize: '48px'}}> É um prazer te conhecer!</h4>
+                    <h4 style={{fontSize: '72px'}}>{"<-"} </h4>
+                </> 
+            }
+
+        </div>
+    );
+}
+
+export default Welcome;
